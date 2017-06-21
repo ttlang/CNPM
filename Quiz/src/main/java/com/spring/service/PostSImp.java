303dea6f08ef1468c06b2ed79baa71d6410b7b0b
ttlang <lang.tt16@gmail.com>
2017-06-21 18:12:24 +0700

    load comment for homepage

--- Quiz/src/main/java/com/spring/service/PostSImp.java
+++ Quiz/src/main/java/com/spring/service/PostSImp.java
@@ -3,7 +3,12 @@ package com.spring.service;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 
@@ -15,6 +20,7 @@ import org.springframework.stereotype.Service;
 import com.spring.domain.Account;
 import com.spring.domain.Comment;
 import com.spring.domain.Post;
+import com.spring.domain.Room;
 import com.spring.repository.PostR;
 
 @Service
@@ -24,6 +30,8 @@ public class PostSImp implements PostS {
 	PostR postR;
 	@Autowired
 	private SessionFactory sf;
+	@Autowired
+	RoomS roomS;
 
 	@Override
 	public List<Comment> getListCommentByIdPost(int idPost) {
@@ -73,6 +81,37 @@ public class PostSImp implements PostS {
 	@Override
 	public List<Account> getListAccountLike(int idPost) {
 		return postR.getListAccountLike(idPost);
+	}
+
+	@Override
+	public List<Post> getPostOfUser(int idAcc) {
+		List<Room> listRoomManage = roomS.getListRoomManager(idAcc);
+		List<Room> listRoomPar = roomS.getListRoomParticipation(idAcc);
+
+		Set<Post> listPostRoom = new HashSet<>();
+		for (Room room : listRoomManage) {
+			if (room.getPostList() != null && !room.getPostList().isEmpty()) {
+				listPostRoom.add(room.getPostList().get(room.getPostList().size() - 1));
+			}
+		}
+
+		for (Room room : listRoomPar) {
+			if (room.getPostList() != null && !room.getPostList().isEmpty()) {
+				listPostRoom.add(room.getPostList().get(room.getPostList().size() - 1));
+			}
+
+		}
+		List<Post>result = new ArrayList<>(listPostRoom);
+		Collections.sort(result,new  Comparator<Post>() {
+
+			@Override
+			public int compare(Post o1, Post o2) {
+					return o2.getPostDate().compareTo(o1.getPostDate());
+			}
+		});
+
+
+		return result;
 	}
 
 }
